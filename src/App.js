8b67c98ade836961f5ffcf3403fdc7e53dad8b4d
kelvine <kchawora@yahoo.com>
2024-06-19 17:23:45 +0200

    added new components

--- src/App.js
+++ src/App.js
@@ -1,44 +1,50 @@
 import React from 'react';
-
 import 'bootstrap/dist/css/bootstrap.min.css';
-
-//Code to import Budget.js
-import Budget from './components/Budget';
-
-// Add code to import the other components here under
 
 
 import { AppProvider } from './context/AppContext';
+import Budget from './components/Budget';
+import Remaining from './components/Remaining';
+import Spent from './components/Spent';
+import ExpenseList from './components/ExpenseList';
+import AllocationForm from './components/AllocationForm';
+import Currencies from './components/Currencies';
+
 const App = () => {
     return (
         <AppProvider>
             <div className='container'>
-                <h1 className='mt-3'>Company's Budget Allocation</h1>
-                    <div className='row mt-3'>
-                        {
-                            /* Add Budget component here */
-                        }        
+            <br />
+            <h1 className='mt-3' style={{ textAlign: 'center' }}>Kelvin's Budget Allocation</h1>
+                <div className='row mt-3'> 
+                    <div className='col-sm'>
+                        <Budget />
+                    </div>
+                    <div className='col-sm'>
+                        <Remaining />
+                    </div>
+                    <div className='col-sm'>
+                        <Spent />
+                    </div>
+                    <div className='col-sm'>
+                        <Currencies />
+                    </div>
+                </div>
 
-                        {
-                            /* Add Remaining component here*/
-                        }        
+<br />
+<br />
 
-                        {
-                            /* Add ExpenseTotal component here */
-                        }        
-                       
-                        {
-                            /* Add ExpenseList component here */
-                        }         
-
-                        {
-                            /* Add ExpenseItem component here */
-                        }        
-
-                        {
-                            /* Add AllocationForm component here under */
-                        }        
-
+                <h3 className='mt-3' style={{ textAlign: 'center' }}>Allocation</h3>
+                <div className='row '>
+                    <div className='col-sm'>
+                        <ExpenseList />
+                    </div>
+                </div>
+                <h3 className='mt-3' style={{ textAlign: 'center' }}>Change Allocation</h3>
+                <div className='row mt-3'>
+                    <div className='col-sm'>
+                        <AllocationForm/>
+                    </div>
                 </div>
             </div>
         </AppProvider>
